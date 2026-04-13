9b2c592c20d93a185cf5a49c15495744e3868576
leandrodosreisfilho-lang <leandrodosreisfilho@gmail.com>
2026-04-13 12:33:50 -0300

    funções funcionando porem vale refatorar

--- modulo/funcoes.js
+++ modulo/funcoes.js
@@ -19,26 +19,28 @@ function getListaDeTudo(){
 
 function getListaDadosProfile(numero){
 
-    
     let conteudo
     let resposta = []
 
     dados['whats-users'].forEach(function(itemUsers){
 
-        if(itemUsers.number === numero)
+        if(itemUsers.number === numero){
 
-        resposta.push({
+        conteudo = {
             "nome" : itemUsers.account,
             "nick" : itemUsers.nickname,
             "foto" : itemUsers['profile-image'],
             "background" : itemUsers.background,
             "criacao_conta" : itemUsers['created-since'].start,
             "termino_conta" : itemUsers['created-since'].end
-        })
+        }
 
+        resposta.push(conteudo)
+    }
     })
 
-    return resposta
+    return conteudo
+    // return resposta
 
 }
 
@@ -89,6 +91,58 @@ function getListaDeMensagens(numero){
 
 // console.log(getListaDeMensagens("11987876567"))
 
-function getListaConversaEntreUsuarioEContato(numero){
+function getListaConversaEntreUsuarioEContato(numero, contato){
 
+    let resposta = false
+
+    dados['whats-users'].forEach(function(itemUsers){
+
+        itemUsers.contacts.forEach(function(itemContato){
+            if(String(itemContato.name).toUpperCase().trim() === String(contato).toUpperCase().trim()){
+                    resposta = {
+                        "name": itemContato.name,
+                        "description" : itemContato.description,
+                        "messages" : itemContato.messages
+                    }
+                }
+        })
+    })
+
+    return resposta
 }
+
+// console.log(getListaConversaEntreUsuarioEContato("11987876567","Jane Smith"))
+
+function filtrarConversaGeral(numero, termo) {
+    let status = false
+    let resposta = []
+
+    dados['whats-users'].forEach(function(itemUsers) {
+        if (String(numero).trim() == String(itemUsers.number).trim()) {
+
+            itemUsers.contacts.forEach(function(itemContato) {
+                itemContato.messages.forEach(function(itemMensagem) {
+
+                    if (itemMensagem.content.includes(termo)) {
+                        status = true
+                        resposta.push(itemMensagem)
+                    }
+
+                })
+            })
+
+        }
+    })
+
+    if (!status) {
+        return false
+    }
+
+    let dadosJsonFiltro = {
+        mensagensFiltradas: resposta
+    }
+
+    return dadosJsonFiltro
+}
+
+console.log(filtrarConversaGeral(11955577796, "bem"))
